e7fb6e4c15c0904e9021646ca7b1d3a801a9577b
weilei <1149327214@qq.com>
2019-04-12 22:20:46 +0800

    add struct

---
+++ DataStruct/DataStruct/SysLib.h
@@ -0,0 +1,9 @@
+//#pragma once
+#ifndef SYSLIB_H_
+#define SYSLIB_H_
+
+#include<stdlib.h>
+#include<stdio.h>
+#include<windows.h>
+
+#endif
---
+++ DataStruct/DataStruct/main.cpp
@@ -0,0 +1,7 @@
+#include "SysLib.h"
+
+
+int main()
+{
+	return 0;
+}
